56b4708dbd4cd001fd981fd4702aa8362843b1fe
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-05 14:36:31 +0100

    Auth Controller in progress

--- src/database/migrations/20200305044515-create-user.js
+++ src/database/migrations/20200305044515-create-user.js
@@ -1,27 +1,54 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Users', {
+			uuid: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+			},
+			name: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			email: {
+				allowNull: false,
+				unique: true,
+				type: Sequelize.STRING
+			},
+			phone: {
+				allowNull: false,
+				type: Sequelize.REAL
+			},
+			username: {
+				allowNull: false,
+				unique: true,
+				type: Sequelize.STRING
+			},
+			password: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			address: {
+				type: Sequelize.STRING
+			},
+			profile_pic: {
+				type: Sequelize.STRING
+			},
+			ref_uuid: Sequelize.UUID,
+			referer_uuid: Sequelize.UUID,
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Users');
+	}
 };
