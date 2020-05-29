05982575c1dc9cd79b8ea41384f2eedb2f1bcefd
Lovisgod <olifedayo94@gmail.com>
2020-05-29 17:06:24 +0100

    bug:stage issues for some accounts

--- src/utils/StageHelper.js
+++ src/utils/StageHelper.js
@@ -59,6 +59,7 @@ const stages = [
 const LastStageCompleted = (downlines) => {
   let stage_completed = 0;
   const dlines = downlines.filter((x) => x.payed);
+  console.log(dlines);
   if (dlines.length == 0) {
       stage_completed = 0
       return stage_completed;
@@ -96,10 +97,11 @@ const LastStageCompleted = (downlines) => {
 };
 
 const getUserStageAndReward = (downlines) => {
-  console.log(downlines);
+  // console.log(downlines);
   const stage_completed = LastStageCompleted(downlines);
+  console.log(stage_completed);
   const stage = stages.filter((x) => x.stage_no == stage_completed)[0];
-  console.log(stage);
+  // console.log(stage);
   return { stage_completed, reward:stage.reward };
 }
 
